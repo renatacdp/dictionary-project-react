b714994450f5c243697d12dfc42128c51f6b9771
Renata Pinto <renata.dib@gmail.com>
2022-11-01 17:41:02 +0000

    Styled the App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">Dictionary</header>
-      <h1>What word do you want to look up?</h1>
+      <h1>What word do you want to look up today?</h1>
+      <Dictionary />
     </div>
   );
 }
