22baa73584e30decc900c035572a4a91399d831b
Renata Pinto <renata.dib@gmail.com>
2022-11-01 12:36:54 +0000

    Cleaning up the file to start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Dictionary</header>
+      <h1>What word do you want to look up?</h1>
     </div>
   );
 }
-
-export default App;
