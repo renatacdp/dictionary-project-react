8de8c68b78421aec9e651a090fa988cc2d3a4edd
Renata Pinto <renata.dib@gmail.com>
2022-11-16 21:47:05 +0000

    Created photos component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,13 +2,19 @@ import axios from "axios";
 import React, { useState } from "react";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
@@ -16,6 +22,13 @@ export default function Dictionary() {
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    const pexelsApiKey =
+      "563492ad6f9170000100000127f8c993d017448a8f19d29f86266d17";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleKeywordChange(event) {
@@ -34,6 +47,7 @@ export default function Dictionary() {
         <button>Search</button>
       </form>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
