eca72e95a61a6586186f73d69bf0d28e26f829ea
Renata Pinto <renata.dib@gmail.com>
2022-11-16 22:03:06 +0000

    Switched container to div

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
-    <container className="App">
+    <div className="App">
       <header className="App-header">Dictionary</header>
       <h1>What word do you want to look up?</h1>
       <Dictionary />
@@ -14,6 +14,6 @@ export default function App() {
         by Renata Pinto 👩‍💻
       </footer>
       <span className="spacing">space</span>
-    </container>
+    </div>
   );
 }
