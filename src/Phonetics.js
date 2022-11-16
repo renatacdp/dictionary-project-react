40bfe46046c8b06d0eeb3289609bd21c50a26bf7
Renata Pinto <renata.dib@gmail.com>
2022-11-16 21:47:23 +0000

    Fixed Phonetics bug

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -2,12 +2,16 @@ import React from "react";
 import "./Phonetics.css";
 
 export default function Phonetics(props) {
-  return (
-    <div className="Phonetics">
-      <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
-        Listen
-      </a>
-      <span>{props.phonetics.text}</span>
-    </div>
-  );
+  if (props.phonetics.audio) {
+    return (
+      <div className="Phonetics">
+        <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
+          Listen
+        </a>
+        <span>{props.phonetics.text}</span>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
