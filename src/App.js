c479fd7ae29c14321be342be9f170fb2964df8b9
Renata Pinto <renata.dib@gmail.com>
2022-11-16 22:48:43 +0000

    Fixed url and styles

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
-    <div className="App">
+    <container className="App">
       <header className="App-header">Dictionary</header>
       <h1>What word do you want to look up?</h1>
       <Dictionary />
       <footer>
-        <a href="https://github.com/renatacdp/dictionary-project-react">
+        <a
+          href="https://github.com/renatacdp/dictionary-project-react"
+          target="_blank"
+          rel="noreferrer"
+        >
           Open-source code
         </a>{" "}
         by Renata Pinto 👩‍💻
       </footer>
       <span className="spacing">space</span>
-    </div>
+    </container>
   );
 }
