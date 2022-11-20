61a8d3668d66e781cff41fcf5daa0d27d588a093
Renata Pinto <renata.dib@gmail.com>
2022-11-20 17:49:36 +0000

    Synonyms text and CSS

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -5,6 +5,7 @@ export default function Synonyms(props) {
   if (props.synonyms.length !== 0) {
     return (
       <div className="Synonyms">
+        <strong>Synonym(s):</strong>
         <ul>
           {props.synonyms.map(function (synonym, index) {
             return <li key={index}>{synonym}</li>;
